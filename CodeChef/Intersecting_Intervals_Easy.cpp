6606aaa27a23e09e1c08519da5c290b16c58d938
SASU004 <shaswataghosh091@gmail.com>
2025-12-03 23:15:06 +0530

    auto update

--- CodeChef/Intersecting_Intervals_Easy.cpp
+++ CodeChef/Intersecting_Intervals_Easy.cpp
@@ -9,33 +9,38 @@ void solution(){
     vector<ll>a(n),b(n);
     loop(0,n) cin>>a[i];
     loop(0,n) cin>>b[i];
-    
-    vector<ll>endA(n),startB(n);
-    
-    ll cur=0;
-    loop(0,n){
-        cur = max(a[i], cur + a[i]);
-        endA[i] = cur;
-    }
-    cur=LLONG_MIN;
-    // reverse kadane
-    ll running=0;
-    for(int i=n-1;i>=0;i--){
-        running = max(b[i], running + b[i]);
-        startB[i] = running;
+
+    vector<ll>ea(n),sa(n),eb(n),sb(n);
+
+    ea[0]=a[0];
+    eb[0]=b[0];
+    for(int i=1;i<n;i++){
+        ea[i]=max(a[i],ea[i-1]+a[i]);
+        eb[i]=max(b[i],eb[i-1]+b[i]);
     }
 
-    ll ans = LLONG_MIN;
-    loop(0,n) ans = max(ans, endA[i] + startB[i]);
-    
-    cout<<ans<<"\n";
+    sa[n-1]=a[n-1];
+    sb[n-1]=b[n-1];
+    for(int i=n-2;i>=0;i--){
+        sa[i]=max(a[i],sa[i+1]+a[i]);
+        sb[i]=max(b[i],sb[i+1]+b[i]);
+    }
+
+    ll ans=LLONG_MIN;
+    loop(0,n){
+        ll bestA=ea[i]+sa[i]-a[i];
+        ll bestB=eb[i]+sb[i]-b[i];
+        ans=max(ans,bestA+bestB);
+    }
+
+    cout<<ans<<endl;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T; 
+    int T=1;
     cin>>T;
     while(T--) solution();
     return 0;
@@ -74,4 +79,3 @@ int main(){
 
 
 */
- 
