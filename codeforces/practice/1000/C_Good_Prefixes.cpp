0e0268a813b28d069dbc917c1a9c9001430d7aa2
SASU004 <shaswataghosh091@gmail.com>
2025-12-06 00:09:41 +0530

    auto update

---
+++ codeforces/practice/1000/C_Good_Prefixes.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(a,n) for(int i = a; i < n; i++)
+#define pb push_back
+
+
+void solution() {
+    int n ;
+    cin>>n;
+   
+   
+   ll sum = 0;
+    int mx = 0, ans = 0;
+for(int i= 0; i < n; i++){
+    int x;
+    cin>>x;
+    sum+= x;
+    mx=max(mx,x);
+    if(sum-mx ==mx) 
+     ans++;
+}
+cout<<ans<<endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1; 
+    cin >> T; // uncomment if multiple test cases
+    while (T--) {
+        solution();
+    }
+    return 0;
+}
+
+
+/*              obsevations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
