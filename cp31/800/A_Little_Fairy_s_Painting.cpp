67a26069830c7936d9811980dc102f5d84657a9f
SASU004 <shaswataghosh091@gmail.com>
2025-12-10 14:13:21 +0530

    auto update

---
+++ cp31/800/A_Little_Fairy_s_Painting.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(a,n) for(int i = a; i < n; i++)
+#define pb push_back
+
+
+
+void solution(){
+    int n; 
+    cin >> n;
+    unordered_set<ll> s;
+    s.reserve(n*2);
+
+    for(int i = 0; i < n; i++){
+        ll x;
+        cin >> x;
+        s.insert(x);
+    }
+
+    ll cur = s.size();
+    while(!s.count(cur)){
+        s.insert(cur);
+        cur++;
+        if(cur > 2e6) break;
+    }
+    cout << cur << endl;
+}
+   
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1; 
+    cin >> T; // uncomment if multiple test cases
+    while (T--) {
+        solution();
+    }
+    return 0;
+}
+
+
+/*              obsevations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
--- usaco guide/bronze/Problem_1_Mixing_Milk.cpp
+++ usaco guide/bronze/Problem_1_Mixing_Milk.cpp
@@ -1,52 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define loop(a,n) for(int i = a; i < n; i++)
+#define loop(a,n) for(int i=a;i<n;i++)
 #define pb push_back
 
-
-void solution() {
-    int n ;
-    cin>>n;
+const int n=3;
+void solution(){
+    ll c[3],m[3];
+    loop(0,3) cin>>c[i]>>m[i];
+    loop(0,100){
+        int a=i%3;
+        int b=(i+1)%3;
+        ll can=c[b]-m[b];
+        ll pour=min(m[a],can);
+        m[a]-=pour;
+        m[b]+=pour;
+    }
+    cout<<m[0]<<endl<<m[1]<<endl<<m[2]<<endl;
 }
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int T = 1; 
-    cin >> T; // uncomment if multiple test cases
-    while (T--) {
+    int T=1;
+    while(T--){
         solution();
     }
     return 0;
 }
-
-
-/*              obsevations
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
