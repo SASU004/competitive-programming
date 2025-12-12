dfe9d6dec54dc0a75781cf8ae8b57d7b09e987df
SASU004 <shaswataghosh091@gmail.com>
2025-12-12 20:08:03 +0530

    auto update

---
+++ cp31/1000/B_Olya_and_Game_with_Arrays.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(a,n) for(int i = a; i < n; i++)
+
+#define pb push_back
+#define ppb pop_back
+#define vi vector<int>
+#define vl vector<long long>
+
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define si set<int>
+#define sll set<long long>
+#define spi set<pii>
+#define spl set<pll>
+
+#define msi multiset<int>
+#define msll multiset<long long>
+
+#define usi unordered_set<int>
+#define usll unordered_set<long long>
+
+#define INF 1e18
+#define MOD 1000000007
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define read_vec(v,n) for(int i=0;i<n;i++) cin>>v[i]
+#define read_set(s,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; s.insert(_x); }
+
+
+#define pqmax priority_queue<ll>
+#define pqmin priority_queue<ll, vector<ll>, greater<ll>>
+
+template<typename T>
+void print_vec(const vector<T> &v){
+    for(auto &x : v) cout << x << " ";
+    cout << "\n";
+}
+
+
+
+void solution() {
+    int n;
+    cin>>n;
+    vl second;ll low_min=INT_MAX;
+    loop(0,n){
+        int m;cin>>m;
+        vl v(m);read_vec(v,m);
+        sort(all(v));
+        low_min=min(low_min,v[0]);
+        second.pb(v[1]);
+    }
+    sort(all(second));
+    ll sum=accumulate(all(second),0LL);
+    ll low_sec=second[0];
+    ll ans=low_min+sum-low_sec;
+    cout<<ans<<endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1; 
+    cin >> T; // uncomment if multiple test cases
+    while (T--) {
+        solution();
+    }
+    return 0;
+}
+
+
+/*              obsevations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
