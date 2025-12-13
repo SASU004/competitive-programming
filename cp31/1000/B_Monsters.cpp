81bb6ad27dd96c9832fc826c7067001065ad3ab5
SASU004 <shaswataghosh091@gmail.com>
2025-12-13 17:41:27 +0530

    auto update

---
+++ cp31/1000/B_Monsters.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(a,n) for(int i = a; i < n; i++)
+
+#define pb push_back
+#define ppb pop_back
+#define vi vector<int>
+#define vl vector<long long>
+
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define si set<int>
+#define sll set<long long>
+#define spi set<pii>
+#define spl set<pll>
+
+#define msi multiset<int>
+#define msll multiset<long long>
+
+#define usi unordered_set<int>
+#define usll unordered_set<long long>
+
+#define INF 1e18
+#define MOD 1000000007
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define read_vec(v,n) for(int i=0;i<n;i++) cin>>v[i]
+#define read_set(s,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; s.insert(_x); }
+
+
+#define pqmax priority_queue<ll>
+#define pqmin priority_queue<ll, vector<ll>, greater<ll>>
+
+template<typename T>
+void print_vec(const vector<T> &v){
+    for(auto &x : v) cout << x << " ";
+    cout << "\n";
+}
+
+
+
+void solution() {
+    ll n,k;
+    cin>>n>>k;
+    vl v(n);
+    read_vec(v,n);
+    vpll ans;
+    loop(0,n){
+        if(v[i]%k==0)ans.pb({k,i+1});
+        else ans.pb({v[i]%k,i+1});
+    }
+    sort(ans.begin(), ans.end(), [](const pll &a, const pll &b){
+    if(a.first != b.first) return a.first > b.first;
+    return a.second < b.second;
+});
+
+    
+    loop(0,n){
+        cout<<ans[i].second<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1; 
+    cin >> T; // uncomment if multiple test cases
+    while (T--) {
+        solution();
+    }
+    return 0;
+}
+
+
+/*              obsevations
+
+learnet  this sort structure not solved by me but i got the intution in 2 mins 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
