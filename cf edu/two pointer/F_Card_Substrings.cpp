ee5ea9791b66c2a23d74b35134a3916da733106d
SASU004 <shaswataghosh091@gmail.com>
2025-12-27 02:00:46 +0530

    auto update

---
+++ cf edu/two pointer/F_Card_Substrings.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(a,n) for(int i = a; i < n; i++)
+#define endl '\n'
+
+#define pb push_back
+#define ppb pop_back
+#define vi vector<int>
+#define vl vector<long long>
+
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define si set<int>
+#define sll set<long long>
+#define spi set<pii>
+#define spl set<pll>
+
+#define msi multiset<int>
+#define msll multiset<long long>
+
+#define usi unordered_set<int>
+#define usll unordered_set<long long>
+
+#define INF 1e18
+#define MOD 1000000007
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define read_vec(v,n) for(int i=0;i<n;i++) cin>>v[i]
+#define read_set(s,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; s.insert(_x); }
+#define read_mset(ms,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; ms.insert(_x); }
+
+
+#define pqmax priority_queue<ll>
+#define pqmin priority_queue<ll, vector<ll>, greater<ll>>
+
+template<typename T>
+void print_vec(const vector<T> &v){
+    for(auto &x : v) cout << x << " ";
+    cout << "\n";
+}
+
+
+
+void solution() {
+    int n , m ; 
+    cin>>n>>m;
+    string s , cards ;
+    cin>>s>>cards;
+    vi freq(26,0),win(26,0);
+    for(char x:cards)freq[x-'a']++;
+    ll ans=0;
+    int l=0;
+    loop(0,n){
+        win[s[i]-'a']++;
+        while(win[s[i]-'a']>freq[s[i]-'a']){
+            win[s[l]-'a']--;
+            l++;
+        }
+        ans+=(i-l+1);
+    }
+    cout<<ans<<endl;
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1; 
+    // cin >> T; // uncomment if multiple test cases
+    while (T--) {
+        solution();
+    }
+    return 0;
+}
+
+
+/*              obsevations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
