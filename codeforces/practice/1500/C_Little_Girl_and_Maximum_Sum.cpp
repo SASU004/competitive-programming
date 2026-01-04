b39df0aadd3a338edeb7f133b610b3d78445a254
SASU004 <shaswataghosh091@gmail.com>
2026-01-04 06:51:06 +0530

    auto update

---
+++ codeforces/practice/1500/C_Little_Girl_and_Maximum_Sum.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(a,n) for(int i = a; i < n; i++)
+#define endl '\n'
+
+#define pb push_back
+#define ppb pop_back
+#define vi vector<int>
+#define vl vector<long long>
+
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define si set<int>
+#define sll set<long long>
+#define spi set<pii>
+#define spl set<pll>
+
+#define msi multiset<int>
+#define msll multiset<long long>
+
+#define usi unordered_set<int>
+#define usll unordered_set<long long>
+
+#define INF 1e18
+#define MOD 1000000007
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define read_vec(v,n) for(int i=0;i<n;i++) cin>>v[i]
+#define read_set(s,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; s.insert(_x); }
+#define read_mset(ms,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; ms.insert(_x); }
+
+
+#define pqmax priority_queue<ll>
+#define pqmin priority_queue<ll, vector<ll>, greater<ll>>
+
+template<typename T>
+void print_vec(const vector<T> &v){
+    for(auto &x : v) cout << x << " ";
+    cout << "\n";
+}
+
+
+
+void solution() {
+    int n,q;
+    cin>>n>>q;
+    vl a(n);
+    read_vec(a,n);
+    
+    vl diff(n+2,0);
+    while( q--){
+       int l,r;
+       cin>>l >>r;
+       l--;r--;
+        diff[l]++;
+        diff[r+1]--;
+    }
+    loop(1,n){
+        diff[i]+=diff[i-1];
+    }
+    sort(rall(a));
+    sort(rall(diff));
+    ll ans=0 ;
+    loop(0,n){
+        ans+=a[i]*diff[i];
+    }
+    cout<<ans <<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1; 
+    // cin >> T; // uncomment if multiple test cases
+    while (T--) {
+        solution();
+    }
+    return 0;
+}
+
+
+/*              obsevations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
