c7ceb78bd322fc7378b7f225c9f0449e34ca98df
SASU004 <shaswataghosh091@gmail.com>
2026-01-08 23:34:29 +0530

    auto update

--- codeforces/practice/1700/D_Matrix_Cascade.cpp
+++ codeforces/practice/1700/D_Matrix_Cascade.cpp
@@ -54,9 +54,60 @@ void print_vec(const vector<T> &v){
 
 
 void solution() {
-    int n ;
-    cin>>n;
-    
+    int n;
+    cin >> n;
+
+    static int a[3005][3005];
+    static int b[3005][3005];
+    static int c[3005][3005];
+
+    // clear arrays
+    loop(0,n+2){
+         for(int j=1;j<=n;j++){
+            a[i][j] = b[i][j] = c[i][j] = 0;
+        }
+    }
+
+    // input
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            char ch;
+            cin >> ch;
+            a[i][j] = ch - '0';
+        }
+    }
+
+    // prefix xor upwards
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n;j++){
+            a[i][j] ^= a[i-1][j];
+        }
+    }
+
+    int ans = 0;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(b[i][j]){
+                a[i][j] ^= 1;
+                b[i+1][j-1] ^= 1;
+            }
+            if(c[i][j]){
+                a[i][j] ^= 1;
+                c[i+1][j+1] ^= 1;
+            }
+        }
+
+        for(int j=1;j<=n;j++){
+            if(a[i][j]){
+                ans++;
+                b[i+1][j-1] ^= 1;
+                c[i+1][j+1] ^= 1;
+            }
+        }
+    }
+
+    cout << ans << endl;
 }
 
 int main() {
@@ -74,6 +125,11 @@ int main() {
 
 /*              obsevations
 
+i got the whole idea of the problem but 
+this problem hasnt been solved by me 
+couldnt code this up
+this is copy pasted just for the sake of learning difference rray 
+but this is more likely a propagation problem 
 
 
 
