ba3960ca756e442fd26c89c298b24727db079b2a
SASU004 <shaswataghosh091@gmail.com>
2026-01-29 23:10:29 +0530

    auto update

---
+++ CodeChef/School_Trip.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(a,n) for(int i = a; i < n; i++)
+#define endl '\n'
+
+#define pb push_back
+#define ppb pop_back
+#define vi vector<int>
+#define vl vector<long long>
+
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define si set<int>
+#define sll set<long long>
+#define spi set<pii>
+#define spl set<pll>
+
+#define msi multiset<int>
+#define msll multiset<long long>
+
+#define usi unordered_set<int>
+#define usll unordered_set<long long>
+
+#define INF 1e18
+#define MOD 1000000007
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define read_vec(v,n) for(int i=0;i<n;i++) cin>>v[i]
+#define read_set(s,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; s.insert(_x); }
+#define read_mset(ms,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; ms.insert(_x); }
+
+
+#define pqmax priority_queue<ll>
+#define pqmin priority_queue<ll, vector<ll>, greater<ll>>
+
+#define lb(v,x) (lower_bound(all(v),x) - (v).begin())   // -> first >= x
+#define ub(v,x) (upper_bound(all(v),x) - (v).begin())   // -> first > x
+#define rlb(v,x) (lower_bound(all(v),x) - (v).begin() - 1)   // -> last < x
+#define rub(v,x) (upper_bound(all(v),x) - (v).begin() - 1)   // -> last <= x
+#define cnt_range(v,l,r) (upper_bound(all(v),r) - lower_bound(all(v),l))
+template<typename T>
+void print_vec(const vector<T> &v){
+    for(auto &x : v) cout << x << " ";
+    cout << "\n";
+}
+/*              obsevations
+
+
+*/
+void solution() {
+    ll n,x,k;
+    cin>>n>>x>>k;
+    ll check=x%k;
+    ll ans =x;
+    ans= min(check,ans);
+    if(x+(k-check)<=n){
+        ans=min(ans,k-check);
+    }
+    cout<<ans<<endl;
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1; 
+    cin >> T; // uncomment if multiple test cases
+    while (T--) {
+            
+        solution();
+    }
+    return 0;
+}
+
+
+
