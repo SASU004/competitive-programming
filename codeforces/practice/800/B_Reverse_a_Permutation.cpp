d80455a978fdf17a6916d173f57adf2a003d0ddb
SASU004 <shaswataghosh091@gmail.com>
2026-02-10 03:12:05 +0530

    auto update

---
+++ codeforces/practice/800/B_Reverse_a_Permutation.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(a,n) for(int i = a; i < n; i++)
+#define endl '\n'
+
+#define pb push_back
+#define ppb pop_back
+#define vi vector<int>
+#define vl vector<long long>
+
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define si set<int>
+#define sll set<long long>
+#define spi set<pii>
+#define spl set<pll>
+
+#define msi multiset<int>
+#define msll multiset<long long>
+
+#define usi unordered_set<int>
+#define usll unordered_set<long long>
+
+#define INF 1e18
+#define MOD 1000000007
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define read_vec(v,n) for(int i=0;i<n;i++) cin>>v[i]
+#define read_set(s,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; s.insert(_x); }
+#define read_mset(ms,n) for(int _i=0,_x; _i<n; _i++){ cin>>_x; ms.insert(_x); }
+
+
+#define pqmax priority_queue<ll>
+#define pqmin priority_queue<ll, vector<ll>, greater<ll>>
+
+#define lb(v,x) (lower_bound(all(v),x) - (v).begin())   // -> first >= x
+#define ub(v,x) (upper_bound(all(v),x) - (v).begin())   // -> first > x
+#define rlb(v,x) (lower_bound(all(v),x) - (v).begin() - 1)   // -> last < x
+#define rub(v,x) (upper_bound(all(v),x) - (v).begin() - 1)   // -> last <= x
+#define cnt_range(v,l,r) (upper_bound(all(v),r) - lower_bound(all(v),l))
+template<typename T>
+void print_vec(const vector<T> &v){
+    for(auto &x : v) cout << x << " ";
+    cout << "\n";
+}
+/*              obsevations
+
+
+*/
+void solution(){
+    int n;
+    cin>>n;
+
+    vi p(n+5);
+    int ind=1;
+
+    loop(1,n+1){
+        cin>>p[i];
+    }
+
+    while(ind<=n && p[ind]==n-ind+1) ind++;
+
+    int id=-1;
+
+    loop(ind,n+1){
+        if(p[i]==n-ind+1) id=i;
+    }
+
+    loop(1,ind){
+        cout<<p[i]<<" ";
+    }
+
+    if(id!=-1){
+        for(int i=id;i>=ind;i--) cout<<p[i]<<" ";
+        loop(id+1,n+1){
+            cout<<p[i]<<" ";
+        }
+    }
+
+    cout<<endl;
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1; 
+    cin >> T; // uncomment if multiple test cases
+    while (T--) {
+            
+        solution();
+    }
+    return 0;
+}
+
+
+
